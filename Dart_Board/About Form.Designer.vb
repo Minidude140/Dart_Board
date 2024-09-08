3545c14176ecd84b9984e03f8e20b9fa9f8de6b4
Zac <zac.minidude140@Gmail.com>
2024-09-08 15:16:25 -0600

    Created about form

---
+++ Dart_Board/About Form.Designer.vb
@@ -0,0 +1,70 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class AboutForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(AboutForm))
+        Me.AbouQuitButton = New System.Windows.Forms.Button()
+        Me.AboutLabel = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'AbouQuitButton
+        '
+        Me.AbouQuitButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.AbouQuitButton.Location = New System.Drawing.Point(133, 168)
+        Me.AbouQuitButton.Name = "AbouQuitButton"
+        Me.AbouQuitButton.Size = New System.Drawing.Size(106, 29)
+        Me.AbouQuitButton.TabIndex = 0
+        Me.AbouQuitButton.Text = "Close"
+        Me.AbouQuitButton.UseVisualStyleBackColor = True
+        '
+        'AboutLabel
+        '
+        Me.AboutLabel.AutoSize = True
+        Me.AboutLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.AboutLabel.Location = New System.Drawing.Point(12, 9)
+        Me.AboutLabel.Name = "AboutLabel"
+        Me.AboutLabel.Size = New System.Drawing.Size(371, 119)
+        Me.AboutLabel.TabIndex = 1
+        Me.AboutLabel.Text = resources.GetString("AboutLabel.Text")
+        Me.AboutLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'AboutForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(395, 209)
+        Me.Controls.Add(Me.AboutLabel)
+        Me.Controls.Add(Me.AbouQuitButton)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "AboutForm"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "About"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents AbouQuitButton As Button
+    Friend WithEvents AboutLabel As Label
+End Class
